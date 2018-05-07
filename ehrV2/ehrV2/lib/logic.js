1bb911c7852782d11c3929772ce0f9ae3cd654af
TyreseLian <tyreselian3915@gmail.com>
2018-05-07 05:24:49 -0400

    Update logic.js

--- ehrV2/ehrV2/lib/logic.js
+++ ehrV2/ehrV2/lib/logic.js
@@ -10,20 +10,27 @@ async function addBaby(newBaby) { // eslint-disable-line no-unused-vars
 
     const baby = factory.newResource(namespace, 'Baby', newBaby.babyId);
     baby.babyId = newBaby.babyId;
-    baby.detail = newBaby.detail;
+    baby.age = newBaby.age;
+    baby.gender = newBaby.gender;
+    baby.DoB = newBaby.DoB;
     baby.mom = factory.newRelationship(namespace, 'Mom', newBaby.mom.getIdentifier());
 
     // save the order
     const assetRegistry = await getAssetRegistry(baby.getFullyQualifiedType());
     await assetRegistry.add(baby);
-/*
-    // emit the event
-    const placeOrderEvent = factory.newEvent(namespace, 'PlaceOrderEvent');
-    placeOrderEvent.orderId = baby.orderId;
-    placeOrderEvent.vehicleDetails = baby.vehicleDetails;
-    placeOrderEvent.options = baby.options;
-    placeOrderEvent.orderer = baby.orderer;
-    emit(placeOrderEvent);*/
+}
+
+async function addMom(newBaby) { // eslint-disable-line no-unused-vars
+
+    const factory = getFactory();
+    const namespace = 'org.acme.vehicle_network';
+
+    const baby = factory.newResource(namespace, 'Mom', newBaby.username);
+    baby.username = newBaby.username
+
+    // save the order
+    const assetRegistry = await getAssetRegistry(baby.getFullyQualifiedType());
+    await assetRegistry.add(baby);
 }
 
 /**
@@ -38,17 +45,11 @@ async function UpdateStatus(updateBaby) { // eslint-disable-line no-unused-vars
     // get vehicle registry
     const vehicleRegistry = await getAssetRegistry(namespace + '.Baby');
     const baby = await vehicleRegistry.get(updateBaby.babyId);
-    baby.detail = updateBaby.detail;
+    baby.age = updateBaby.age;
     await vehicleRegistry.update(baby);
 }
 
-/*    // emit the event
-    const updateOrderStatusEvent = factory.newEvent(namespace, 'UpdateOrderStatusEvent');
-    updateOrderStatusEvent.orderStatus = update.order.orderStatus;
-    updateOrderStatusEvent.order = update.order;
-    emit(updateOrderStatusEvent);(/
-}
-
+/*
  * Update the status of an order
  * @param {org.acme.vehicle_network.addImmunizations} immunization - the immunization transaction
  * @transaction
@@ -62,10 +63,36 @@ async function addImmunizations(immunization) { // eslint-disable-line no-unused
     const baby = await vehicleRegistry.get(immunization.babyId);
     baby.immunizations.push(immunization.vaccines);
     await vehicleRegistry.update(baby);
+}
 
-/*    // emit the event
-    const updateOrderStatusEvent = factory.newEvent(namespace, 'UpdateOrderStatusEvent');
-    updateOrderStatusEvent.orderStatus = update.order.orderStatus;
-    updateOrderStatusEvent.order = update.order;
-    emit(updateOrderStatusEvent);*/
+/*
+ * Update the status of an order
+ * @param {org.acme.vehicle_network.addExaminationNote} note) - the immunization transaction
+ * @transaction
+ */
+async function addExaminationNote(note) { // eslint-disable-line no-unused-vars
+    const factory = getFactory();
+    const namespace = 'org.acme.vehicle_network';
+
+    // get vehicle registry
+    const vehicleRegistry = await getAssetRegistry(namespace + '.Baby');
+    const baby = await vehicleRegistry.get(note.babyId);
+    baby.examinationNote.push(note.note);
+    await vehicleRegistry.update(baby);
+}
+
+/*
+ * Update the status of an order
+ * @param {org.acme.vehicle_network.nextCheckUp} date) - the immunization transaction
+ * @transaction
+ */
+async function nextCheckUp(date) { // eslint-disable-line no-unused-vars
+    const factory = getFactory();
+    const namespace = 'org.acme.vehicle_network';
+
+    // get vehicle registry
+    const vehicleRegistry = await getAssetRegistry(namespace + '.Baby');
+    const baby = await vehicleRegistry.get(date.babyId);
+    baby.age = date.date;
+    await vehicleRegistry.update(baby);
 }
