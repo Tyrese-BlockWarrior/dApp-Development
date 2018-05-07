b6afbfa821b4cbf534003c24af7619336398ef8d
TyreseLian <tyreselian3915@gmail.com>
2018-05-07 13:57:32 -0400

    changed_h1

--- ehr_0.0.4/angular-app/proxy.conf.js
+++ ehr_0.0.4/angular-app/proxy.conf.js
@@ -24,7 +24,7 @@ function getTarget() {
         const restServerURL = process.env.REST_SERVER_URL;
         return restServerURL;
     }
-    return 'http://localhost:3000';
+    return 'https://1eadab42.ngrok.io'
 }
 
 const target = getTarget();
